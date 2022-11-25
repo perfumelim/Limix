700906de88fefac72e450a0aaf2f052d42ab9c31
perfumelim <perfumelim@gmail.com>
2022-11-25 11:52:06 +0900

    🎉

---
+++ app/components/Sample.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Sample = () => {
+  return <Block>BLUE</Block>;
+};
+
+export default Sample;
+
+const Block = styled.div`
+  background: blue;
+  width: 100px;
+  height: 100px;
+`;
