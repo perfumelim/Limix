53da1bd1e1d115bb56c997c5a170aef862d031cf
perfumelim <perfumelim@gmail.com>
2022-11-28 11:14:21 +0900

    뒤로가기 버튼 기능 구현 및 커스텀 컬러 추가

--- app/components/HeaderBackButton.tsx
+++ app/components/HeaderBackButton.tsx
@@ -1,9 +1,13 @@
 import { ArrowBack } from './vectors'
 
-const HeaderBackButton = () => {
+interface Props {
+  onClick?: () => void
+}
+
+const HeaderBackButton = ({ onClick }: Props) => {
   return (
-    <button className="text-customGrey-400 ">
-      <ArrowBack />
+    <button className="text-customGrey-400 h-full p-2 -ml-2 flex items-center" onClick={onClick}>
+      <ArrowBack className="w-7 h-auto stroke-customGrey-300 fill-customGrey-300" />
     </button>
   )
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,13 +2,13 @@ import Header from '~/components/Header'
 import HeaderBackButton from '~/components/HeaderBackButton'
 import { useGoBack } from '~/hooks/useGoBack'
 
-const Resister = () => {
+const Login = () => {
   const goBack = useGoBack()
   return (
     <div>
-      <Header title="회원가입" headerBack={<HeaderBackButton onClick={goBack} />} />
+      <Header title="로그인" headerBack={<HeaderBackButton onClick={goBack} />} />
     </div>
   )
 }
 
-export default Resister
+export default Login
