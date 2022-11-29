d1ba5ae563f86894420eec007c12ba04451ae351
perfumelim <perfumelim@gmail.com>
2022-11-29 12:06:24 +0900

    feat: completes AuthForm UI

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -6,25 +6,38 @@ interface Props {
   mode: 'login' | 'register'
 }
 
+const authDescriptions = {
+  login: {
+    usernamePlaceholder: '아이디를 입력하세요',
+    passwordPlaceholder: '비밀번호를 입력하세요',
+    buttonText: '로그인',
+    actionText: '회원가입',
+    question: '계정이 없으신가요?',
+    actionLink: '/register',
+  },
+  register: {
+    usernamePlaceholder: '5~20자 영문, 숫자 입력',
+    passwordPlaceholder: '8자 이상, 영문/숫자/특수문자 중 2가지 이상 입력',
+    buttonText: '회원가입',
+    actionText: '로그인',
+    question: '계정이 이미 있으신가요?',
+    actionLink: '/login',
+  },
+} as const
+
 const AuthForm = ({ mode }: Props) => {
-  const isRegister = mode === 'register'
+  const { usernamePlaceholder, passwordPlaceholder, buttonText, actionLink, actionText, question } =
+    authDescriptions[mode]
   return (
     <div className="h-full p-4 flex flex-col justify-between">
       <div className="flex flex-col gap-4 w-full">
-        <LabelInbult
-          label="아이디"
-          placeholder={isRegister ? '5~20자 영문, 숫자 입력' : '아이디를 입력하세요'}
-        />
-        <LabelInbult
-          label="비밀번호"
-          placeholder={
-            isRegister ? '8자 이상, 영문/숫자/특수문자 중 2가지 이상 입력' : '비밀번호를 입력하세요'
-          }
-        />
+        <LabelInbult label="아이디" placeholder={usernamePlaceholder} />
+        <LabelInbult label="비밀번호" placeholder={passwordPlaceholder} />
       </div>
       <div className="flex flex-col items-center gap-y-6">
-        <Button layoutMode="fullWidth">회원가입</Button>
-        <QuestionLink question="계정이 이미 있으신가요?" name="로그인" to="/login" />
+        <Button layoutMode="fullWidth">{buttonText}</Button>
+
+        <QuestionLink question={question} name={actionText} to={actionLink} />
       </div>
     </div>
   )
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,3 +1,4 @@
+import AuthForm from '~/components/AuthForm'
 import Header from '~/components/Header'
 import HeaderBackButton from '~/components/HeaderBackButton'
 import { useGoBack } from '~/hooks/useGoBack'
@@ -5,8 +6,9 @@ import { useGoBack } from '~/hooks/useGoBack'
 const Login = () => {
   const goBack = useGoBack()
   return (
-    <div>
+    <div className="h-full">
       <Header title="로그인" headerBack={<HeaderBackButton onClick={goBack} />} />
+      <AuthForm mode="login" />
     </div>
   )
 }
